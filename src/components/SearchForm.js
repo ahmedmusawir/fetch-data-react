e0874a18aa5f81444c2aed10d07d95d2d15ff49e
Ahmed Musawir <moose@Mooses-High-Sierra.local>
2018-08-14 19:33:46 +0800

    Fetch & Axios tested - ongoing component separation - 14 Aug 2018

---
+++ src/components/SearchForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SearchForm extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Search Form</h1>
+      </div>
+    );
+  }
+}
+
+export default SearchForm;
